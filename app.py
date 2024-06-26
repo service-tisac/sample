9241fecb13e24fa57a35780a31009d7ff07513be
Tisac <service@tisac.in>
2024-06-26 13:19:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
-from flask import Flask
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/hello')
-def hello():
-    return 'Привет'
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-@app.route('/goodbye')
-def goodbye():
-    return 'До свидания'
-
-@app.route('/goodmorning')
-def goodmorning():
-    return 'Доброе утро'
-
-@app.route('/goodevening')
-def goodevening():
-    return 'Добрый вечер'
-
-@app.route('/goodnight')
-def goodnight():
-    return 'Спокойной ночи'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8888)
+@app.get("/greet/{name}")
+def greet(name: str):
+    return {"message": f"Hello, {name} inside the container i hope."}
